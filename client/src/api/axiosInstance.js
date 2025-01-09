a5e6a84609a3653737aeb4a2d9cd439a540c3ff6
HarshithaNaladala <hnaladala11@gmail.com>
2025-01-09 13:33:39 -0500

    changed vite config

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://lms-learn-git-main-harshitha-naladalas-projects.vercel.app/',
+    baseURL: 'http://localhost:5000',
 });
 
 axiosInstance.interceptors.request.use(config => {
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,21 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-const __filename = fileURLToPath(import.meta.url); 
-const __dirname = path.dirname(__filename); 
+// const __filename = fileURLToPath(import.meta.url); 
+// const __dirname = path.dirname(__filename); 
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  build:{
+    outDir : 'dist'
+  }
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, './src'),
+  //   },
+  // },
 });
 
 
